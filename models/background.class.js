abc00815b6a2133532178c37de724981d984a3f5
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-07-08 21:48:37 +0200

    Renamed folder and removed unused classes.

--- models/background.class.js
+++
@@ -1,9 +0,0 @@
-class Background extends MovableObject {
-  width = 850;
-  height = 480;
-  constructor(imagePath, x) {
-    super().loadImage(imagePath);
-    this.x = x;
-    this.y = 0;
-  }
-}
--- models/midground.class.js
+++
@@ -1,9 +0,0 @@
-class Midground extends MovableObject {
-  width = 720;
-  height = 200;
-  constructor(imagePath, x) {
-    super().loadImage(imagePath);
-    this.x = x;
-    this.y = 300;
-  }
-}
