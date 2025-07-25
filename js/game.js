82ccd50eedf6a84195a91f7315df659789c14953
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-07-25 15:16:43 +0200

    Started working on Fullscreen mode

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let intervalIds = [];
+const fullscreen = document.getElementById("fullscreen");
 
 /**
  * Initializes the game world by selecting the canvas element
@@ -9,8 +11,72 @@ let keyboard = new Keyboard();
  * Called once at the start to set up the environment.
  */
 function init() {
+  initLevel();
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
+}
+
+async function enterFullscreen(element) {
+  try {
+    if (element.requestFullscreen) {
+      await element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+
+    element.classList.add("fullscreen-active");
+    resizeCanvasToFullscreen();
+  } catch (error) {
+    console.warn("Fullscreen could not be activated", error);
+  }
+}
+
+async function exitFullscreen() {
+  if (document.fullscreenElement) {
+    await document.exitFullscreen();
+  } else if (document.webkitFullscreenElement) {
+    document.webkitExitFullscreen();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  if (!document.fullscreenElement) {
+    fullscreen.classList.remove("fullscreen-active");
+  }
+});
+
+document.addEventListener("webkitfullscreenchange", () => {
+  if (!document.webkitFullscreenElement) {
+    fullscreen.classList.remove("fullscreen-active");
+  }
+});
+
+function resizeCanvasToFullscreen() {
+  canvas.width = 720;
+}
+
+function resetCanvasSize() {
+  canvas.width = 720;
+}
+
+function resizeCanvas() {
+  if (document.fullscreenElement) {
+    resizeCanvasToFullscreen();
+  } else {
+    resetCanvasSize();
+  }
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+function stopGame() {
+  clearAllIntervals();
+}
+
+function clearAllIntervals() {
+  for (let i = 1; i < 9999; i++) window.clearInterval(i);
 }
 
 document.addEventListener("keydown", (event) => {
@@ -36,6 +102,10 @@ document.addEventListener("keydown", (event) => {
 
   if (event.code === "KeyD") {
     keyboard.D = true;
+  }
+
+  if (event.code === "Escape") {
+    exitFullscreen();
   }
 });
 
