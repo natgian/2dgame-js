6a91c6dc40afb023310f9e6c4476b96c9a8fb81e
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-09-17 23:40:16 +0200

    Added JS Doc

--- models/firefly.class.js
+++ models/firefly.class.js
@@ -29,7 +29,11 @@ class Firefly extends MovableObject {
   /**
    * Animates the firefly.
    *
-   * - x and y positions are updated every frame
+   * - Updates x and y positions 60 times per second
+   * - Moves the object according to its velocityX and velocityY
+   * - Adds a small random vertical movement to make motion less linear
+   * - Vertical speed stays within -0.7 and 0.7 to prevent the object from moving too fast vertically
+   * - Wraps the object to the left when it passes the end of the level
    *
    */
   animate() {
@@ -37,8 +41,8 @@ class Firefly extends MovableObject {
       this.x += this.velocityX;
       this.y += this.velocityY;
 
-      this.velocityY += (Math.random() - 0.5) * 0.1;
-      this.velocityY = Math.max(Math.min(this.velocityY, 0.7), -0.7);
+      this.velocityY += (Math.random() - 0.5) * 0.1; // between -0.05 and 0.05
+      this.velocityY = Math.max(Math.min(this.velocityY, 0.7), -0.7); // between -0.7 and 0.7
 
       if (this.x > this.level.levelEndX + 50) {
         this.x = -Math.random() * 200;
